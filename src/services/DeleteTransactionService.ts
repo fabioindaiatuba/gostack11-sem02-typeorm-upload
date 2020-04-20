bd9accbb5e5175b4daf078dcd3b3a5e6ee67c48d
Fabio Gonçalves <fabioindaiatuba@outlook.com>
2020-04-20 00:31:50 -0300

    Create add, delete, list e import transaction

--- src/services/DeleteTransactionService.ts
+++ src/services/DeleteTransactionService.ts
@@ -1,8 +1,21 @@
-// import AppError from '../errors/AppError';
+import { getCustomRepository } from 'typeorm';
+import { isUuid } from 'uuidv4';
+
+import AppError from '../errors/AppError';
+import TransactionRepository from '../repositories/TransactionsRepository';
 
 class DeleteTransactionService {
-  public async execute(): Promise<void> {
-    // TODO
+  public async execute(id: string): Promise<void> {
+    if (!isUuid(id)) {
+      throw new AppError('Invalid id format');
+    }
+    const transactionRepository = getCustomRepository(TransactionRepository);
+    const transaction = await transactionRepository.findOne(id);
+
+    if (!transaction) {
+      throw new AppError('Transaction not found', 404);
+    }
+    await transactionRepository.remove(transaction);
   }
 }
 
